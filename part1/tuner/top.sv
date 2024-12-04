35550cb26ba362c450593775aa9b696bb7008123
Dustin Richmond <6378612+drichmond@users.noreply.github.com>
2024-12-04 14:10:57 -0800

    Fix PLL params in top.sv

--- part1/tuner/top.sv
+++ part1/tuner/top.sv
@@ -81,8 +81,8 @@ module top
     #(.FEEDBACK_PATH("SIMPLE")
      ,.PLLOUT_SELECT("GENCLK")
      ,.DIVR(4'b0000)
-     ,.DIVF(7'b1000011)
-     ,.DIVQ(3'b101)
+     ,.DIVF(7'd59)
+     ,.DIVQ(3'd5)
      ,.FILTER_RANGE(3'b001)
      )
    pll_inst
